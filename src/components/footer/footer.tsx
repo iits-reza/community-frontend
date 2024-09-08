e19cfb4ebfd2d153464d7cea317c21be198d196c
rezamohdev <mohammadireza29@gmail.com>
2024-09-08 16:24:44 -0400

    responsive: footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -9,14 +9,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Footer() {
   return (
-    <footer className="bg-[#F0F4E6] p-[80px] flex flex-col gap-5" id="footer">
+    <footer className="bg-[#F0F4E6] p-[80px] flex flex-col gap-7" id="footer">
       <h2 className="text-[23px] font-title border-primary border-b-2 pb-4">
         <FontAwesomeIcon icon={faLeaf} fontSize={25} className="mr-2" />
         The Community
       </h2>
-      <div className="flex justify-between">
+      <div className="flex flex-col justify-between">
         <span>©️ Charithy 2024, All right reserved </span>
-        <ul className="flex gap-1">
+        <ul className="flex gap-1 justify-center lg:justify-end">
           <li className="hover:bg-primary hover:text-[#f0f4fe] p-4 cursor-pointer">
             <FontAwesomeIcon icon={faXTwitter} fontSize={25} />
           </li>
